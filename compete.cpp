2b7da7589f0f313624ccf88a9f8d12bb831df091
zimmy9537 <73545703+zimmy9537@users.noreply.github.com>
2022-10-02 22:57:25 +0530

    hacktoberfest-2022
    hacktoberfest-2022

--- compete.cpp
+++ compete.cpp
@@ -33,6 +33,43 @@ int maxWater(int arr[], int n)
     return res;
 }
 
+
+int maxProfit(int price[], int start, int end)
+{
+ 
+    // If the stocks can't be bought
+    if (end <= start)
+        return 0;
+ 
+    // Initialise the profit
+    int profit = 0;
+ 
+    // The day at which the stock
+    // must be bought
+    for (int i = start; i < end; i++) {
+ 
+        // The day at which the
+        // stock must be sold
+        for (int j = i + 1; j <= end; j++) {
+ 
+            // If buying the stock at ith day and
+            // selling it at jth day is profitable
+            if (price[j] > price[i]) {
+ 
+                // Update the current profit
+                int curr_profit
+                    = price[j] - price[i]
+                      + maxProfit(price, start, i - 1)
+                      + maxProfit(price, j + 1, end);
+ 
+                // Update the maximum profit so far
+                profit = max(profit, curr_profit);
+            }
+        }
+    }
+    return profit;
+}
+
 int main()
 {
 	int arr[5]={1,2,3,4,5};
